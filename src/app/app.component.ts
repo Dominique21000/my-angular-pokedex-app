1d45772d84fbc429c90299a0eff55caad8efbba5
Dominique SAUVIGNON <dominique.sauvignon@outlook.fr>
2024-08-24 17:55:42 +0200

    initialisation of the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  name = 'Pikachu';
+  life= 21;
+
+  incrementLife(){
+    this.life = this.life + 1;
+  }
+
+  decrementLife(){
+    this.life = this.life - 1;
+  }
+}
